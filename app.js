0126977636afa93bfdf2c0461580eddc386969ea
ArbutusNate <arbutusnate@gmail.com>
2018-06-22 15:05:44 -0700

    little navbar added, want to customize animations. scrolling nicely to targets with custom JS

--- app.js
+++ app.js
@@ -1,33 +1,9 @@
-function isElementInViewport(elem) {
-    var $elem = $(elem);
-
-    // Get the scroll position of the page.
-    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
-    var viewportTop = $(scrollElem).scrollTop();
-    var viewportBottom = viewportTop + $(window).height();
-
-    // Get the position of the element on the page.
-    var elemTop = Math.round( $elem[0].getBoundingClientRect().y );
-
-    return (elemTop < viewportBottom);
-}
-
-// Check if it's time to start the animation.
-function checkAnimation() {
-    var $elem = $('.gogo');
-
-    // If the animation has already been started
-    if ($elem.hasClass('ran')) return;
-
-    if (isElementInViewport($elem)) {
-        // Start the animation
-        console.log("starting animation")
-        $elem.addClass('ran animated slideInLeft');
-    }
-}
-
-// Capture scroll events
-$(window).scroll(function(){
-    // isElementInViewport('#about');
-    checkAnimation();
-});
+$(window).ready(function(){
+    $("button").click(function(e) {
+        let name = $(this).attr("data-name");
+        let destination = $(name);
+        $("html, body").animate({
+            scrollTop: destination.offset().top
+        }, 1500)
+    })
+})
