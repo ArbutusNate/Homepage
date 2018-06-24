a33882f6f23e738fde791398b423f17dd9e29f50
ArbutusNate <arbutusnate@gmail.com>
2018-06-24 13:37:48 -0700

    fixed for mobile

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 $(window).ready(function(){
-    $("button").click(function(e) {
-        let name = $(this).attr("data-name");
-        let destination = $(name);
-        $("html, body").animate({
-            scrollTop: destination.offset().top
-        }, 1500)
-    })
+  $("button").click(function(e) {
+    let name = $(this).attr("data-name");
+    let destination = $(name);
+    $("html, body").animate({
+        scrollTop: destination.offset().top
+    }, 1500)
+  })
 })
