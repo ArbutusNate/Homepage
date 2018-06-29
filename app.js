347b5924c17c04b872e1c2832399b0c8a27614ff
ArbutusNate <arbutusnate@gmail.com>
2018-06-29 14:57:58 -0700

    animated top banner with jQuery to reveal octocat

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 let hoverOn = function() {
   $(this).animate({
-    right: "+=59",
-    top: "+=50"
+    right: "+=50",
+    top: "+=40"
   })
+  $(".gh-mark").css("visibility", "visible").hide().fadeIn(400)
 }
 
 let hoverOff = function () {
   $(this).animate({
-    right: "-=59",
-    top: "-=50"
-  })
+    right: "-=50",
+    top: "-=40"
+  });
+  // $(".gh-mark").fadeTo(400, 0);
+  $(".gh-mark").fadeOut(400, () => {
+  $(".gh-mark").css("visibility", "hidden").css("display", "")
+  });
 }
 
 
 
 
 $(window).ready(function(){
+  ($(window).width() < 800) ?
+  console.log('small')  :
+  console.log('big');
+  AOS.init();
+
   $("button").click(function(e) {
     let name = $(this).attr("data-name");
     let destination = $(name);
@@ -26,12 +36,6 @@ $(window).ready(function(){
   $("#top-right-ribbon").hover(hoverOn, hoverOff)
 })
 
-$(window).ready(function(){
-    if ($(window).width() < 800) {
-      console.log('small');
-      return;
-    } else {
-      console.log('big');
-      AOS.init();
-    }
-})
+// $(window).ready(function(){
+
+// })
