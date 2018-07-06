524f1c3e22930a0b12faa6104238bca6650a8098
ArbutusNate <arbutusnate@gmail.com>
2018-07-06 12:25:17 -0700

    improving jquery/javascript for full screen

--- app.js
+++ app.js
@@ -1,35 +1,73 @@
+let btnClick = function () {
+  let name = $(this).attr("data-name");
+  let destination = $(name);
+  $("html, body").animate({
+      scrollTop: destination.offset().top
+  }, 1500)
+};
+
 let hoverGitOn = function() {
-  $(this).animate({
-    right: "+=50",
-    top: "+=40"
-  })
-  $(".gh-mark").css("visibility", "visible").hide().fadeIn(400)
-}
+  let _banner = $(this);
+  if(_banner.attr("status") === "hidden" && _banner.attr("action") === "stopped") {
+    _banner.attr({
+      status : "shown",
+      action : "animating"}
+    ).animate({
+      right: "+=50",
+      top: "+=40"
+    }, 1000, () => {
+      _banner.attr("action", "stopped");
+    })
+    $(".gh-mark").css("visibility", "visible").hide().fadeIn(400)
+  } else {
+    console.log('Either shown or Animating');
+    return;
+  }
+};
 
 let hoverGitOff = function () {
-  $(this).animate({
-    right: "-=50",
-    top: "-=40"
-  });
-  $(".gh-mark").fadeOut(400, () => {
-  $(".gh-mark").css("visibility", "hidden").css("display", "")
-  });
-}
+  let _banner = $(this);
+  if(_banner.attr("status") === "shown" && _banner.attr("action") === "stopped") {
+    _banner.attr({
+      status : "hidden",
+      action: "animating"}
+      ).animate({
+      right: "-=50",
+      top: "-=40"
+      }, 1000, () => {
+        _banner.attr("action", "stopped");
+      });
+    $(".gh-mark").fadeOut(400, () => {
+    $(".gh-mark").css("visibility", "hidden").css("display", "")
+    });
+  } else {
+    console.log('Either hidden or Animating');
+
+    return;
+  }
+};
 
 let hoverPicOn = function () {
-  console.log($(this).width());
+  if ($(this).attr("data-action") === "stopped") {
+    console.log('stopped');
     let oWidth = $(this).width();
-    $(this).animate({
+    $(this).attr("data-action", "animating").animate({
       width : "0px",
       "border-width": "1px 0px 1px 10px"
     }, 600, () => {
-        $(".flip-front").toggle();
-        $(".flip-back").toggle();
-        $(this).animate({
-            width: oWidth,
-            "border-width": "1px 1px 1px 1px"
-        }, 600)
+      $(".flip-front").toggle();
+      $(".flip-back").toggle();
+      $(this).animate({
+          width: oWidth,
+          "border-width": "1px 1px 1px 1px"
+      }, 600, () => {
+        $(this).attr("data-action", "stopped");
+      })
     })
+  } else {
+    console.log('animating');
+    return;
+  }
 }
 
 // let hoverPicOff = function () {
@@ -40,31 +78,13 @@ let hoverPicOn = function () {
 // }
 
 $(window).ready(function(){
-  if ($(window).width() < 800) {
-    console.log('small');
-    // $("#profile-pic").css("src", "./assets/me_pic_640.jpg");
-  } else {
-    console.log('big');
-    // $("#profile-pic").css("src", "./assets/me_pic_900.jpg");
-    AOS.init();
-  }
-  // ($(window).width() < 800) ?
-  // // console.log('small')
-  // $("#profile-pic").css("src", "./assets/me_pic_640.jpg")  :
-  // // console.log('big');
-  // $("#profile-pic").css("src", "./assets/me_pic_900.jpg")
 
-  $("button").click(function(e) {
-    let name = $(this).attr("data-name");
-    let destination = $(name);
-    $("html, body").animate({
-        scrollTop: destination.offset().top
-    }, 1500)
-  })
-  $("#top-right-ribbon").hover(hoverGitOn, hoverGitOff)
-  // $(".flip").on("mouseenter", hoverPicOn)
+  ($(window).width() < 800) ?
+  console.log('small') :
+  console.log('big');
+  AOS.init();
+
+  $("button").on('click', btnClick);
+  $("#top-right-ribbon").hover(hoverGitOn, hoverGitOff);
+  $(".flip").on('mouseenter', hoverPicOn);
 })
-
-// $(window).ready(function(){
-
-// })
